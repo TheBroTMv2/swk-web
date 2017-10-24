d89443fb22ebaedffcb25637672825f8250d1c61
thebrotm <tobias-mueller@outlook.de>
2017-10-24 20:39:22 +0200

    Code cleanup
    nur den müll rausgebracht ;-)

--- index.php
+++ index.php
@@ -1,60 +1,79 @@
 <!doctype html>
 <html>
+
 <head>
-
-
-<meta charset="utf-8">
-<meta name="description" content="Wir arbeiten momentan an einer kompletten Überarbeitung unserer Internetpräsenz.">
-<meta name="keywords" content="Schriesheimer Weinkeller,Schriesheim,Weinhandel,Wein,Weinkeller,Baden-Württemberg,Region,Einzelhandel">
-<link rel="shortcut icon" href="img/favicon.png">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Schriesheimer WEINKELLER</title>
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
+	<meta charset="utf-8">
+	<meta name="description" content="Wir arbeiten momentan an einer kompletten Überarbeitung unserer Internetpräsenz.">
+	<meta name="keywords" content="Schriesheimer Weinkeller,Schriesheim,Weinhandel,Wein,Weinkeller,Baden-Württemberg,Region,Einzelhandel">
+	<link rel="shortcut icon" href="img/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Schriesheimer WEINKELLER</title>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 </head>
 
 <body style="padding-top: 60px">
-  <?php require("nav.php"); ?>
+	<?php require("nav.php"); ?>
+	<div class="row main1">
+		<div class="col-sm-5 col-md-7 col-md-offset-1 col-xs-9">
+			<h1 class="h11">Schriesheimer WEINKELLER</h1>
+			<p><img class="img-responsive pull-left imgspacer" src="img/wine-186018_1920.png" width="240px" alt="404 Weinflasche not found ;-)"/>Das ist ein austauschbarer Text der zum Beispiel das Unternehmen mit ein paar Wörtern beschreiben könnte. Auch könnte man hier wissenswertes schreiben und wie man sieht auch ein Bild anzeigen lassen. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p><br/><br/>
+			<h3 class="h11">Unterüberschrift</h3>
+			<p>
+				Hier kann noch mehr text stehen und ein weiteres Bild. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br/><br/><br/> sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Hier kann noch mehr text stehen und ein weiteres Bild. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				<br/>
+			</p>
+		</div>
 
+		<div class="col-xs-4 col-md-3 ">
+			<h2 class="h11">Beispielüberschrift 2</h2>
+			<p>Hier an der Seite kann man z.B: Neuigkeiten anzeigen lassen oder ein Bewertungsfeed etc. <img class="img-responsive pull-right imgspacer img-rounded" src="img/bordeaux-150955.png" width="180px" alt="404 Weinflasche not found ;-)"/>
+			</p><br/>
+		</div>
 
-<div class="row main1">
-  <div class="col-sm-5 col-md-7 col-md-offset-1 col-xs-9"><h1 class="h11">Schriesheimer WEINKELLER</h1>
-<p><img class="img-responsive pull-left imgspacer" src="img/wine-186018_1920.png" width="240px" alt="404 Weinflasche not found ;-)" />Das ist ein austauschbarer Text der zum Beispiel das Unternehmen mit ein paar Wörtern beschreiben könnte. Auch könnte man hier wissenswertes schreiben und wie man sieht auch ein Bild anzeigen lassen. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p><br /><br /><h3 class="h11">Unterüberschrift</h3><p>
-Hier kann noch mehr text stehen und ein weiteres Bild. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br /><br /><br /> sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-Hier kann noch mehr text stehen und ein weiteres Bild. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
- <br />
- </p>
- 
- </div>
-  
-	<div class="col-xs-4 col-md-3 "><h2 class="h11">Beispielüberschrift 2</h2><p>Hier an der Seite kann man z.B: Neuigkeiten anzeigen lassen oder ein Bewertungsfeed etc. <img class="img-responsive pull-right imgspacer img-rounded" src="img/bordeaux-150955.png" width="180px" alt="404 Weinflasche not found ;-)" /></p><br /></div>
+	</div>
 
-</div>
-
-<div class="row">
-	<div class="footer11 col-xs-18 col-sm-4 text-center "><p class="footer1"><h3>Footer 1</h3></p></div>
-  <div class="col-xs-18 col-sm-4 text-center footer11"><p class="footer2" ><h3>Footer 2</h3></p></div>
-  <!-- Optional: Setze die Floats der XS-Spalten zurück, falls ihr Inhalt nicht gleich hoch ist -->
-  <div class="clearfix visible-xs-block"></div>
-	<div class="col-xs-18 col-sm-4 text-center footer11"><p class="footer3"><h3>Social Media</h3><!--<div class="fb-page" data-href="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/" data-tabs="timeline" data-width="350" data-height="320" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/">Schriesheimer Weinkeller</a></blockquote></div>--></p></div>
-</div>
-<link href="css/bootstrap-3.3.7.css" rel="stylesheet" type="text/css">
-<script src="js/jquery-1.11.3.min.js"></script>
-<!-- <script src="js/bootstrap.js"></script> -->
-<script src="js/bootstrap-3.3.7.js"></script>
-<link href="css/style.css" rel="stylesheet" >
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.10&appId=414054425405713';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+	<div class="row">
+		<div class="footer11 col-xs-18 col-sm-4 text-center ">
+			<p class="footer1">
+				<h3>Footer 1</h3>
+			</p>
+		</div>
+		<div class="col-xs-18 col-sm-4 text-center footer11">
+			<p class="footer2">
+				<h3>Footer 2</h3>
+			</p>
+		</div>
+		<!-- Optional: Setze die Floats der XS-Spalten zurück, falls ihr Inhalt nicht gleich hoch ist -->
+		<div class="clearfix visible-xs-block"></div>
+		<div class="col-xs-18 col-sm-4 text-center footer11">
+			<p class="footer3">
+				<h3>Social Media</h3>
+				<!--<div class="fb-page" data-href="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/" data-tabs="timeline" data-width="350" data-height="320" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Schriesheimer-Weinkeller-2013185778937160/">Schriesheimer Weinkeller</a></blockquote></div>-->
+			</p>
+		</div>
+	</div>
+	<link href="css/bootstrap-3.3.7.css" rel="stylesheet" type="text/css">
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<!-- <script src="js/bootstrap.js"></script> -->
+	<script src="js/bootstrap-3.3.7.js"></script>
+	<link href="css/style.css" rel="stylesheet">
+	<div id="fb-root"></div>
+	<script>
+		( function ( d, s, id ) {
+			var js, fjs = d.getElementsByTagName( s )[ 0 ];
+			if ( d.getElementById( id ) ) return;
+			js = d.createElement( s );
+			js.id = id;
+			js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.10&appId=414054425405713';
+			fjs.parentNode.insertBefore( js, fjs );
+		}( document, 'script', 'facebook-jssdk' ) );
+	</script>
 </body>
+
 </html>
