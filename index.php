2e641f4354de212670121a7dfc22be6f5056b4e2
thebrotm <tobias-mueller@outlook.de>
2017-10-24 20:28:00 +0200

    Exluded Menu to external file
    Das Menü wird über php eingebunden so muss nicht auf jeder einzelnen page der Inhalt des menüs geändert werden wenn eine neue seite hinzukommt. Und man spart etwas code insgesammt (wobei hierbei auch wieder die frage ist lohnt es sich)

--- index.php
+++ index.php
@@ -19,33 +19,7 @@
 </head>
 
 <body style="padding-top: 60px">
-  <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-        <a class="navbar-brand" href="#"><img class="img-circle img-thumbnail" src="img/logoswk_klein.png" width="50px" alt="Schriesheimer WEINKELLER" /></a></div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="topFixedNavbar1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home<span class="sr-only">(current)</span></a></li>
-			<li><a href="#"><span class="glyphicon glyphicon-glass"></span>Sortiment</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span>Über uns</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-envelope" ></span>Kontakt</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-road" ></span>Anfahrt</a></li>
-        </ul>
-        <form class="navbar-form navbar-right" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        
-      </div>
-      <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container-fluid -->
-  </nav>
+  <?php require("nav.php"); ?>
 
 
 <div class="row main1">
